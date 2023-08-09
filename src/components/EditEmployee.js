70be9ac07bfa2f140c2fe733794b717642d044b5
Carlos Figueroa <carfig7186@students.ecpi.edu>
2023-08-08 21:33:43 -0400

    undo changes

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -93,241 +93,229 @@ const EditEmployee = () => {
         <div className="my-4">
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col gap-3">
-              <div className="flex">
-                <div className="flex flex-col items-center gap-3">
-                  <label className="text-xl t:text-2xl">
-                    Employee Information
+              <div className="ml-1 t:flex t:justify-center">
+                <label className="text-xl t:text-2xl">
+                  Employee Information
+                </label>
+              </div>
+              <div className="flex flex-col items-center gap-3">
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Name:
                   </label>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      Name:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Full Name"
-                      id="name"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, name: e.target.value })
-                      }
-                      value={data.name}
-                      autoComplete="off"
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      Email:
-                    </label>
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      id="email"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, email: e.target.value })
-                      }
-                      value={data.email}
-                      autoComplete="off"
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      Date of Birth:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Date of Birth"
-                      id="dob"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, dob: e.target.value })
-                      }
-                      value={data.dob}
-                      autoComplete="off"
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      Phone Number:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Phone Number"
-                      id="phone"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, phone: e.target.value })
-                      }
-                      value={data.phone}
-                      autoComplete="off"
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      Street Address:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Street Address"
-                      id="street"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, address: e.target.value })
-                      }
-                      value={data.address}
-                      autoComplete="off"
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      City:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="City"
-                      id="city"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, city: e.target.value })
-                      }
-                      autoComplete="off"
-                      value={data.city}
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      State:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="State"
-                      id="state"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, state: e.target.value })
-                      }
-                      autoComplete="off"
-                      value={data.state}
-                    ></input>
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="font-bold font-quicksand text-gray">
-                      Zip Code:
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Zip Code"
-                      id="zip"
-                      className="w-80 rounded p-0.5"
-                      onChange={(e) =>
-                        setData({ ...data, zip: e.target.value })
-                      }
-                      autoComplete="off"
-                      value={data.zip}
-                    ></input>
-                  </div>
+                  <input
+                    type="text"
+                    placeholder="Full Name"
+                    id="name"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) => setData({ ...data, name: e.target.value })}
+                    value={data.name}
+                    autoComplete="off"
+                  ></input>
                 </div>
-                <div>
-                  <div className="ml-1 t:flex t:justify-center t:mt-10">
-                    <label className="text-xl font-roboto font-semibold t:text-2xl">
-                      Employment Information
-                    </label>
-                  </div>
-                  <div className="flex flex-col items-center gap-3">
-                    <div className="flex flex-col">
-                      <label className="font-bold font-quicksand text-gray">
-                        Job Title:
-                      </label>
-                      <input
-                        placeholder="Job Title"
-                        id="job"
-                        className="w-80 rounded p-0.5"
-                        onChange={(e) =>
-                          setData({ ...data, job: e.target.value })
-                        }
-                        autoComplete="off"
-                        value={data.job}
-                      ></input>
-                    </div>
-                    <div className="flex flex-col">
-                      <label className="font-bold font-quicksand text-gray">
-                        Department:
-                      </label>
-                      <input
-                        placeholder="Department"
-                        id="department"
-                        className="w-80 rounded p-0.5"
-                        onChange={(e) =>
-                          setData({ ...data, department: e.target.value })
-                        }
-                        autoComplete="off"
-                        value={data.department}
-                      ></input>
-                    </div>
-                    <div className="flex flex-col">
-                      <label className="font-bold font-quicksand text-gray">
-                        Manager:
-                      </label>
-                      <input
-                        placeholder="Manager"
-                        id="manager"
-                        className="w-80 rounded p-0.5"
-                        onChange={(e) =>
-                          setData({ ...data, manager: e.target.value })
-                        }
-                        autoComplete="off"
-                        value={data.manager}
-                      ></input>
-                    </div>
-                    <div className="flex flex-col">
-                      <label className="font-bold font-quicksand text-gray">
-                        Salary:
-                      </label>
-                      <input
-                        placeholder="Salary"
-                        id="salary"
-                        className="w-80 rounded p-0.5"
-                        onChange={(e) =>
-                          setData({ ...data, salary: e.target.value })
-                        }
-                        autoComplete="off"
-                        value={data.salary}
-                      ></input>
-                    </div>
-                    <div className="flex flex-col">
-                      <label className="font-bold font-quicksand text-gray">
-                        Employment Status:
-                      </label>
-                      <input
-                        placeholder="Employment Status"
-                        id="status"
-                        className="w-80 rounded p-0.5"
-                        onChange={(e) =>
-                          setData({ ...data, status: e.target.value })
-                        }
-                        autoComplete="off"
-                        value={data.status}
-                      ></input>
-                    </div>
-                    <div className="flex flex-col">
-                      <label className="font-bold font-quicksand text-gray">
-                        Project:
-                      </label>
-                      <input
-                        placeholder="Project Name (Optional)"
-                        id="project"
-                        className="w-80 rounded mb-4 p-0.5"
-                        onChange={(e) =>
-                          setData({ ...data, project: e.target.value })
-                        }
-                        autoComplete="off"
-                        value={data.project}
-                      ></input>
-                    </div>
-                  </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Email:
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    id="email"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, email: e.target.value })
+                    }
+                    value={data.email}
+                    autoComplete="off"
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Date of Birth:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Date of Birth"
+                    id="dob"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) => setData({ ...data, dob: e.target.value })}
+                    value={data.dob}
+                    autoComplete="off"
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Phone Number:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Phone Number"
+                    id="phone"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, phone: e.target.value })
+                    }
+                    value={data.phone}
+                    autoComplete="off"
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Street Address:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Street Address"
+                    id="street"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, address: e.target.value })
+                    }
+                    value={data.address}
+                    autoComplete="off"
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    City:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="City"
+                    id="city"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) => setData({ ...data, city: e.target.value })}
+                    autoComplete="off"
+                    value={data.city}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    State:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="State"
+                    id="state"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, state: e.target.value })
+                    }
+                    autoComplete="off"
+                    value={data.state}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Zip Code:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Zip Code"
+                    id="zip"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) => setData({ ...data, zip: e.target.value })}
+                    autoComplete="off"
+                    value={data.zip}
+                  ></input>
                 </div>
               </div>
 
+              <div className="ml-1 t:flex t:justify-center t:mt-10">
+                <label className="text-xl font-roboto font-semibold t:text-2xl">
+                  Employment Information
+                </label>
+              </div>
+              <div className="flex flex-col items-center gap-3">
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Job Title:
+                  </label>
+                  <input
+                    placeholder="Job Title"
+                    id="job"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) => setData({ ...data, job: e.target.value })}
+                    autoComplete="off"
+                    value={data.job}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Department:
+                  </label>
+                  <input
+                    placeholder="Department"
+                    id="department"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, department: e.target.value })
+                    }
+                    autoComplete="off"
+                    value={data.department}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Manager:
+                  </label>
+                  <input
+                    placeholder="Manager"
+                    id="manager"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, manager: e.target.value })
+                    }
+                    autoComplete="off"
+                    value={data.manager}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Salary:
+                  </label>
+                  <input
+                    placeholder="Salary"
+                    id="salary"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, salary: e.target.value })
+                    }
+                    autoComplete="off"
+                    value={data.salary}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Employment Status:
+                  </label>
+                  <input
+                    placeholder="Employment Status"
+                    id="status"
+                    className="w-80 rounded p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, status: e.target.value })
+                    }
+                    autoComplete="off"
+                    value={data.status}
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label className="font-bold font-quicksand text-gray">
+                    Project:
+                  </label>
+                  <input
+                    placeholder="Project Name (Optional)"
+                    id="project"
+                    className="w-80 rounded mb-4 p-0.5"
+                    onChange={(e) =>
+                      setData({ ...data, project: e.target.value })
+                    }
+                    autoComplete="off"
+                    value={data.project}
+                  ></input>
+                </div>
+              </div>
               <div className="flex justify-center">
                 <button
                   type="submit"
